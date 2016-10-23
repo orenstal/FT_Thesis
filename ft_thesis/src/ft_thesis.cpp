b3cc73f91626ccc26f4bb65e49c5bdb339dcd364
Tal <Tal@orenstal>
2016-10-23 11:25:56 +0300

    first commen

---
+++ ft_thesis/src/ft_thesis.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : ft_thesis.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!! my comment
+	return 0;
+}
